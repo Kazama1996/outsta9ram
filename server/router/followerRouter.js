e44b0da1046bceb055950a6ee22ba65514774c33
Y.kazama <Y.Kazama1996@gmail.com>
2023-04-07 15:35:19 +0800

    feature: refactor the naming rule of the back router url

--- server/router/followerRouter.js
+++ server/router/followerRouter.js
@@ -9,12 +9,19 @@ const {
 } = require("../controller/followerController");
 const followerRouter = express.Router();
 // follow/unfollow a user.
-followerRouter.post("/api/followers/:profileName", protect, followUser);
-followerRouter.delete("/api/followers/:profileName", protect, unfollowUser);
+followerRouter.post("/api/v1/followers", protect, followUser);
+followerRouter.delete("/api/v1/followers", protect, unfollowUser);
 // get follower and following list of a user.
-followerRouter.get("/api/followers/:profileName/:page", protect, getFollower);
-followerRouter.get("/api/following/:profileName/:page", protect, getFollowing);
+followerRouter.get(
+  "/api/v1/followers/:profileName/:pageNum",
+  protect,
+  getFollower
+);
+followerRouter.get(
+  "/api/v1/following/:profileName/:pageNum",
+  protect,
+  getFollowing
+);
 // check the user is already be followed.
-followerRouter.get("/api/followers/:profileName", protect, isAlreadyFollowed);
 
 module.exports = followerRouter;
